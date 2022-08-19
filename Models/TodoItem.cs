5bc3b6aa26bb1cf1b1a64fd5b64aec9bd7acab4e
Sticky1705 <phamdoanhiep32@gmail.com>
2022-08-19 15:54:20 +0700

    backup

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace TodoApi.Models
+{   
+    public class TodoItem
+    {
+        public int id {get; set;}
+        public string name {get; set;}
+        public bool isComplete {get; set;}
+    }
+}
